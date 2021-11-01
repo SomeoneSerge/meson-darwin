b8ae793b7d29104b63344501b5490782f8e425bb
Someone Serge <sergei.kozlukov@aalto.fi>
2021-11-01 20:43:30 +0200

    try no meson

--- release.nix
+++ release.nix
@@ -1,20 +1,16 @@
 { lib
 , stdenv
-, pkgconfig
-, meson
-, ninja
 }:
 
 stdenv.mkDerivation {
   pname = "hello";
   version = "0.0.1";
   src = ./.;
-  nativeBuildInputs = [ ninja meson pkgconfig ];
-  buildInputs = [
-  ];
-
-  mesonAutoFeatures = "auto";
-
-  # LDFLAGS = lib.optionalString stdenv.isDarwin "-Wl,-L${libstdcxxClang}/lib -Wl,-L${libcxx}/lib";
-  mesonBuildType = "release";
+  buildPhase = ''
+    c++ app.cpp -lstdc++ -o ./hello
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    install ./hello $out/bin/hello
+  '';
 }
